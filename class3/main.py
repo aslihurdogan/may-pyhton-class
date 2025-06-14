844bb54f56e6fee4738ef094c4dba6371dd93e8a
aslihurdogan <aslihurdogan123@gmail.com>
2025-06-14 22:51:17 +0000

    some changes

--- class3/main.py
+++
@@ -1,6 +0,0 @@
-limit=int(input("Enter limit: "))
-count=0
-while count < limit:
-    print(count)
-    count += 1
-
--- variable.py
+++
@@ -1,5 +0,0 @@
-name="Tim"
-print("Hello " + name)
-age=21
-print("You are " +  str(age))
-print("You are", age)
